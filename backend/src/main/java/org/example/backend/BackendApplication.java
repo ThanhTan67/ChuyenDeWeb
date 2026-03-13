c2dfd1c9841a01a2007279058b325b95680c2013
ThanhTan67 <thanhtan8900@gamil.com>
2026-03-13 11:21:31 +0700

    add screenshots

--- backend/src/main/java/org/example/backend/BackendApplication.java
+++ backend/src/main/java/org/example/backend/BackendApplication.java
@@ -3,6 +3,22 @@ package org.example.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@SpringBootApplication
+//public class BackendApplication {
+//    public static void main(String[] args) {
+//        // Load file .env từ thư mục backend
+//        Dotenv dotenv = Dotenv.configure()
+//                .directory("backend")
+//                .load();
+//
+//        // Đưa tất cả biến từ .env vào System properties để Spring Boot đọc được
+//        dotenv.entries().forEach(entry ->
+//                System.setProperty(entry.getKey(), entry.getValue())
+//        );
+//
+//        SpringApplication.run(BackendApplication.class, args);
+//    }
+//}
 @SpringBootApplication
 public class BackendApplication {
     public static void main(String[] args) {
--- backend/src/main/java/org/example/backend/user/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/user/config/SecurityConfig.java
@@ -64,9 +64,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
 
-        config.setAllowedOrigins(List.of(
-                "http://localhost:3000"
-        ));
+        config.setAllowedOriginPatterns(List.of("*"));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
         ));
