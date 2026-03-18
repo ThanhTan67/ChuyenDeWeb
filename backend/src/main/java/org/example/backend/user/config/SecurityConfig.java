0bc4e270b8b2e3ea035a47c0d20d4f731633a3ee
Cao Thành Tân <146425659+ThanhTan67@users.noreply.github.com>
2026-03-18 19:59:10 +0700

    Update SecurityConfig.java

--- backend/src/main/java/org/example/backend/user/config/SecurityConfig.java
+++ backend/src/main/java/org/example/backend/user/config/SecurityConfig.java
@@ -63,9 +63,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-          config.setAllowedOrigins(List.of(
-        "https://aranoz-shop.vercel.app",
-        "http://localhost:3000"));
+          config.setAllowedOrigins(List.of("*"));
         config.setAllowedMethods(List.of(
                 "GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"
         ));
